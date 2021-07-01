c8b9f6cfa9c00f7953b18b33ee887f9fba19f3b4
Akira-Ito0109 <syobonnito@gmail.com>
2021-07-01 16:22:20 +0900

    Unityちゃんの移動まで

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 4f;
+    private Rigidbody2D rigidbody2D;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        if(x!=0)
+        {
+            rigidbody2D.velocity = new Vector2(x * speed, rigidbody2D.velocity.y);
+            Vector2 temp = transform.localScale;
+            temp.x = x;
+            transform.localScale = temp;
+            anim.SetBool("Dash", true);
+        }
+
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0, rigidbody2D.velocity.y);
+            anim.SetBool("Dash", false);
+        }
+    }
+}
